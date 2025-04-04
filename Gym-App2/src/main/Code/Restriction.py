c92321d8aaa9008ccd62c8e05924c33d0cbb025b
MythicBlade <85843085+MythicBlade@users.noreply.github.com>
2025-04-04 10:42:07 -0400

    copied in all of the converted files. Also made a tests folder for future tests.

---
+++ Gym-App2/src/main/Code/Restriction.py
@@ -0,0 +1,23 @@
+class Restriction:
+    def __init__(self, is_vegan, is_vegetarian, allergies):
+        self.is_vegan = is_vegan
+        self.is_vegetarian = is_vegetarian
+        self.allergies = allergies
+
+    def get_vegan(self):
+        return self.is_vegan
+
+    def get_vegetarian(self):
+        return self.is_vegetarian
+
+    def get_allergies(self):
+        return self.allergies
+
+    def change_vegan(self, is_vegan):
+        self.is_vegan = is_vegan
+
+    def change_vegetarian(self, is_vegetarian):
+        self.is_vegetarian = is_vegetarian
+
+    def add_allergy(self, allergy):
+        self.allergies.append(allergy)
---
+++ Gym-App2/src/main/Code/Week.py
@@ -0,0 +1,7 @@
+# This function creates the week that can be viewed in planner, typically this is created by planner and holds
+# all the choices that have been selected for the week
+
+# I am not 100% sure that is the correct way to convert this so I would double check before using it - Owen
+class Week:
+    def __init__(self, choices_list):
+        self.choices = choices_list
