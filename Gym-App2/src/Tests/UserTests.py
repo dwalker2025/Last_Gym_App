678ce4922fbaf1d35267817d9e71707325bd2c83
PJayJay <pfield@ithaca.edu>
2025-04-18 10:35:39 -0400

    User fully tested

--- Gym-App2/src/Tests/UserTests.py
+++ Gym-App2/src/Tests/UserTests.py
@@ -1,27 +1,27 @@
 import unittest
-from edu import User
+import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.user = User(70, 150)
+        self.user = User.User(70, 150)
         self.user.goals = []  # Equivalent to Java's `new ArrayList<>()`
 
     def test_change_weight(self):
-        self.user.changeWeight(180)
+        self.user.change_weight(180)
         self.assertEqual(self.user.weight, 180)
 
-        self.user.changeWeight(200)
+        self.user.change_weight(200)
         self.assertEqual(self.user.weight, 200)
 
     def test_change_height(self):
-        self.user.changeHeight(75)
+        self.user.change_height(75)
         self.assertEqual(self.user.height, 75)
 
-        self.user.changeHeight(80)
+        self.user.change_height(80)
         self.assertEqual(self.user.height, 80)
 
     def test_update_availability(self):
-        self.user.updateAvailability(5)
+        self.user.update_availability(5)
         self.assertEqual(self.user.avail, 5)
 
 if __name__ == '__main__':
