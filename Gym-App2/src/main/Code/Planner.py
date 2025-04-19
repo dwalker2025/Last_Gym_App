a2555068568bbae30f2264acab37796a04723e6c
PJayJay <pfield@ithaca.edu>
2025-04-19 12:31:25 -0400

    Uneeded test classes removed

--- Gym-App2/src/main/Code/Planner.py
+++ Gym-App2/src/main/Code/Planner.py
@@ -1,11 +1,12 @@
 class Planner:
-    def __init__(self, user, map, workouts,diet,current_week,history):
+    def __init__(self, user, map, workouts, diet, current_week, history):
         self.user=user #creates the user to be planned for
-        self.map=map
-        self.workouts=workouts
-        self.diet=diet
-        self.current_week=current_week
-        self.history=history
+        self.map=map #int[] map for measurements in graph
+        self.workouts=workouts #list of possibel workouts for this person
+        self.diet=diet #list of possible foods for this person
+        self.current_week=current_week #The created list using the two last variables
+        self.history=history #History list for each past week
+        self.body_history=[]
 
     def getFoods(self):
         return self.diet
@@ -15,21 +16,21 @@ class Planner:
     
     def showCalendar(self):
         if self.workouts and self.diet:
-            self.map[0]=[self.workouts[0],self.diet[0]]
+            self.map.append([self.workouts[0],self.diet[0]])
             return self.map
         else:
-            self.map[0]=["Salmon","Burpees"]
+            self.map.append(["Salmon","Burpees"])
             return self.map
 
-
-        
-
     def log_state(self):
-        #For testing, adds current height and weight to the map
-        pass
+       this_week=[self.user.get_height(),self.user.get_weight()]
+       self.body_history.append(this_week)
+       self.workouts=[]
+       self.food=[]
+
 
     def make_map(self):
-        #For testing, Resets the curent map and logs current state in refreshed map
+        
         pass
 
     def get_history(self):
